579395c5eeb7ee79d1d2c57482b190e1197f77cd
Taylor <taylorlowe7@gmail.com>
2014-12-10 13:29:21 -0800

    code for controller

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+// public/core.js
+var app = angular.module('todo', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
